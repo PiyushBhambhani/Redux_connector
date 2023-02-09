d9c4d8ccb063a60499668cb982d4e1504bf37e99
Piyush <bhambhanipiyush4@gmail.com>
2023-02-09 17:12:25 +0530

    Adding state , store

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const redux = require('redux')
+const CreateStore= redux.legacy_createStore;
 const BUY_CAKE ='BUY_CAKE'
 function buyCake(){
     return {
@@ -8,8 +10,11 @@ function buyCake(){
 //(previousState,Action) => newState
 // we already have the action defined, so let's define what our state looks like
 const initialState={
-    numOfCakes:10
+    try:20,
+    do:40,
+    numOfCakes:8
 }
+// console.log(initialState);
 
 const reducer=(state= initialState, action)=>{
     switch(action.type){ 
@@ -21,3 +26,21 @@ const reducer=(state= initialState, action)=>{
             default: return state;
     }
 }
+// console.log(reducer(initialState,buyCake()));
+//responsibilty 1 done Holds application state by Attaching itself to a reducer
+const store = CreateStore(reducer)
+// responsibilty 2 (Allows access to state via getState() )
+console.log("Initial state ", store.getState());
+//responsibility 4 (Registers listeners via subscribe(listener))
+const unsubscribe= store.subscribe(()=>{
+    console.log('Updated state is :',store.getState());
+})
+//responsibilty 3: Allows state modification via dispatch(ation)
+// store provides a dispacth method to update a state
+store.dispatch(buyCake());
+// to cause few more state transitions , we can call it multiple times
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+//responsibilty 5 Handles unregestering of listeners by the function which is returned by subscribe(listener)
+unsubscribe();
